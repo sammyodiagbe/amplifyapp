c8d050aac6acd0e1b46e3633a7c6c0174b465912
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2021-07-05 15:07:54 +0100

    making little changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Believe in yourself and everything would work out fine.</p>
+        <p>Let's get this going again.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
