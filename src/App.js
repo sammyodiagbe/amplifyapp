76ab671fa407774e861a34b97c9053dd2969d9e4
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2021-07-08 02:39:06 +0100

    commits and changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Let's get this going again.</p>
+        <p>Building things that smatter.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
